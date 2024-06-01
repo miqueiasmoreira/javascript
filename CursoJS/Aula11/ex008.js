2351569bbab794d452be5e2245baaabcfee69f55
Miquéias Moreira <89392888+miqueiasmoreira@users.noreply.github.com>
2024-06-01 17:06:45 -0300

    Aula 11 + Exercícios

---
+++ CursoJS/Aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel}Km`)
+
+if (vel <= 60.5) {
+    console.log('Dirija sempre usando cinto de segurança!')
+} else {
+    console.log('Você ultrapassou a velocidade permitida! Multado!!!')
+}

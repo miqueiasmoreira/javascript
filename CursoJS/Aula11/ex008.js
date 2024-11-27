e55e811d719e33f5a0d5f1f58547abc627c8f201
Miquéias Moreira <miqueiasmoreirasouza@gmail.com>
2024-11-27 00:22:01 -0300

    Update

--- CursoJS/Aula11/ex008.js
+++
@@ -1,8 +0,0 @@
-var vel = 60.5
-console.log(`A velocidade do seu carro é ${vel}Km`)
-
-if (vel <= 60.5) {
-    console.log('Dirija sempre usando cinto de segurança!')
-} else {
-    console.log('Você ultrapassou a velocidade permitida! Multado!!!')
-}
--- CursoJS/Aula11/ex009.js
+++
@@ -1,7 +0,0 @@
-var país = 'EUA'
-console.log(`Vivendo em ${país}`)
-if (país == 'BR') {
-    console.log('Você é Brasileiro!')
-} else {
-    console.log('Você é estrangeiro!')
-}
